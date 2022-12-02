6603ac7b9b6f83c88af856a57e02206d19d0861c
Econo <nova16797@gmail.com>
2022-12-02 08:01:42 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="logo512.png" className="App-logo" alt="logo" />
+        <img src="logo512.jpg" className="App-logo" alt="logo" />
         <p>
-          What's Up Dophie
+          What's up Dophie
         </p>
       </header>
     </div>
