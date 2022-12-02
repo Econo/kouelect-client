1b8bac1982ec0ebe9070f6c0f9e2b356a25f5da8
Econo <nova16797@gmail.com>
2022-12-02 06:44:28 -0600

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo512.png" className="App-logo" alt="logo" />
+        <p>
+          What's Up Dophie
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
