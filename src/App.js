a2032c63be82998eec7c19294bfa00f6b57c4cf0
Econo <nova16797@gmail.com>
2023-04-26 22:49:43 -0500

    app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="logo512.jpg" className="App-logo" alt="logo" />
         <p>
-          What's up Cute Fart
+          What's up Lilo
         </p>
       </header>
     </div>
